80dda945ae84051bbfe6083afd7eab435ac89233
Matt Harrison <fittocode@gmail.com>
2020-06-18 19:48:49 -0400

    add keydown functionality

--- index.js
+++ index.js
@@ -3,52 +3,118 @@
 //     alert('You clicked me! Hehe');
 //   });
 // });
-var audio = new Audio('sounds/tom-1.mp3');
 
-var allDrums = document.querySelectorAll('.drum')
+var drumMap = {
+  w: 'kick-bass',
+  a: 'snare',
+  s: 'tom-1',
+  d: 'tom-2',
+  j: 'tom-3',
+  k: 'tom-4',
+  l: 'crash',
+};
 
-for (var i = 0; i < allDrums.length; i++) {
-  allDrums[i].addEventListener('click', function () {
-    var buttonInnerHTML = this.innerHTML;
+document.addEventListener('keydown', function (event) {
+  if (event.key) {
+    var hit = new Audio(`sounds/${drumMap[event.key]}.mp3`);
+    hit.play();
+  }
+});
 
-    switch (buttonInnerHTML) {
-      case 'w':
-        var kickHit = new Audio('sounds/kick-bass.mp3');
-        kickHit.play();
-        break;
+// document.addEventListener('keydown', function (event) {
+//   switch (event.key) {
+//     case 'w':
+//       var kickHit = new Audio('sounds/kick-bass.mp3');
+//       kickHit.play();
+//       break;
+//     case 'a':
+//       var snareHit = new Audio('sounds/snare.mp3');
+//       snareHit.play();
+//       break;
 
-      case 'a':
-        var snareHit = new Audio('sounds/snare.mp3');
-        snareHit.play();
-        break;
+//     case 's':
+//       var tom1Hit = new Audio('sounds/tom-1.mp3');
+//       tom1Hit.play();
+//       break;
 
-      case 's':
-        var tom1Hit = new Audio('sounds/tom-1.mp3');
-        tom1Hit.play();
-        break;
+//     case 'd':
+//       var tom2Hit = new Audio('sounds/tom-2.mp3');
+//       tom2Hit.play();
+//       break;
 
-      case 'd':
-        var tom2Hit = new Audio('sounds/tom-2.mp3');
-        tom2Hit.play();
-        break;
+//     case 'j':
+//       var tom3Hit = new Audio('sounds/tom-3.mp3');
+//       tom3Hit.play();
+//       break;
 
-      case 'j':
-        var tom3Hit = new Audio('sounds/tom-3.mp3');
-        tom3Hit.play();
-        break;
+//     case 'k':
+//       var tom4Hit = new Audio('sounds/tom-4.mp3');
+//       tom4Hit.play();
+//       break;
 
-      case 'k':
-        var tom4Hit = new Audio('sounds/tom-4.mp3');
-        tom4Hit.play();
-        break;
+//     case 'l':
+//       var cymbalHit = new Audio('sounds/crash.mp3');
+//       cymbalHit.play();
+//       break;
 
-      case 'l':
-        var cymbalHit = new Audio('sounds/crash.mp3');
-        cymbalHit.play();
-        break;
+//     default:
+//       break;
+//   }
+// });
 
-      default:
-        break;
-    }
-  });
-}
+// var allDrums = document.querySelectorAll('.drum');
+
+// var drumKeys = this.keyboardEvent.key;
+
+// switch (drumKeys) {
+//   case 'k':
+//     var kickHit = new Audio('sounds/kick-bass.mp3');
+//     kickHit.play();
+//     break;
+// }
+
+// for (var i = 0; i < allDrums.length; i++) {
+//   allDrums[i].addEventListener('click', function () {
+//     var buttonInnerHTML = this.innerHTML;
+
+//     switch (buttonInnerHTML) {
+//       case 'w':
+//         var kickHit = new Audio('sounds/kick-bass.mp3');
+//         kickHit.play();
+//         break;
+
+//       case 'a':
+//         var snareHit = new Audio('sounds/snare.mp3');
+//         snareHit.play();
+//         break;
+
+//       case 's':
+//         var tom1Hit = new Audio('sounds/tom-1.mp3');
+//         tom1Hit.play();
+//         break;
+
+//       case 'd':
+//         var tom2Hit = new Audio('sounds/tom-2.mp3');
+//         tom2Hit.play();
+//         break;
+
+//       case 'j':
+//         var tom3Hit = new Audio('sounds/tom-3.mp3');
+//         tom3Hit.play();
+//         break;
+
+//       case 'k':
+//         var tom4Hit = new Audio('sounds/tom-4.mp3');
+//         tom4Hit.play();
+//         break;
+
+//       case 'l':
+//         var cymbalHit = new Audio('sounds/crash.mp3');
+//         cymbalHit.play();
+//         break;
+
+//       default:
+//         break;
+//     }
+//   });
+// }
